47c46bbb37d9241693de68f369b4e50655a36f0c
yanggautier <yangguole@outlook.com>
2020-01-30 13:38:22 +0100

    Project

---
+++ Project/Project/Models/CommentView.swift
@@ -0,0 +1,20 @@
+//
+//  Comment.swift
+//  Project
+//
+//  Created by Guole YANG on 30/01/2020.
+//  Copyright © 2020 Guole YANG. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct Commentaire{
+    
+    var userId: Int
+    var writer: String
+    var postDate: Date
+    var content: String
+    
+}
---
+++ Project/Project/Models/Traveler.swift
@@ -0,0 +1,26 @@
+//
+//  User.swift
+//  Project
+//
+//  Created by Guole YANG on 30/01/2020.
+//  Copyright © 2020 Guole YANG. All rights reserved.
+//
+
+import Foundation
+
+struct Traveler {
+    
+    var firstName: String
+    var lastName: String
+    var location: String
+    var interets: [Interet]
+    
+}
+
+enum Interet{
+    
+    case music
+    case sport
+    case eat
+    case shop
+}
