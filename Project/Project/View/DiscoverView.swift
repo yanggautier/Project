59e8c4c7d5ae0dc364250cbc0702e7f9b8b7a51d
yanggautier <yangguole@outlook.com>
2020-02-05 17:28:06 +0100

    v2

---
+++ Project/Project/View/DiscoverView.swift
@@ -0,0 +1,21 @@
+//
+//  DiscoverView.swift
+//  Project
+//
+//  Created by Guole YANG on 05/02/2020.
+//  Copyright © 2020 Guole YANG. All rights reserved.
+//
+
+import SwiftUI
+
+struct DiscoverView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DiscoverView_Previews: PreviewProvider {
+    static var previews: some View {
+        DiscoverView()
+    }
+}
